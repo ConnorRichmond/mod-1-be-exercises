5d02928aeeda0029ebcf7456398c9ef499cd1409
Connor Richmond <richmond.connor@gmail.com>
2023-05-23 11:39:39 -0400

    Complete nested and warmup

--- lessons/nested_collections/Examples/nested_arrays.rb
+++ lessons/nested_collections/Examples/nested_arrays.rb
@@ -1,7 +1,17 @@
 numbers = [[1, 2, 3], [4, 5, 6]]
 
 # 1. what is numbers.count
+# how many elements in the array
+# in this case 2 arrays in an array
+p numbers.count
 # 2. what is numbers.first.count
+# the amount of elements in the first array
+p numbers.first.count
 # 3. how can I access the element 5
+p numbers[1][1]
 # 4. how can I add [7,8,9] to the numbers array
+numbers << [7, 8, 9]
+p numbers
 # 5. What element does numbers[0] return?
+# the first array in the arrays
+p numbers[0]
--- lessons/nested_collections/Examples/warm_up.rb
+++ lessons/nested_collections/Examples/warm_up.rb
@@ -1,11 +1,29 @@
 animals = {"dogs" => 3, "cats" => 5, "iguanas" => 2}
 
+p animals['dogs']
+
+animals['parakeets'] = 3
+p animals
+
+animals['cats'] = 7
+p animals
+
+
 # 1. return the value of dogs
 # 2. add 3 parakeets
 # 3. increase the amount of cats by 2
 
 pet_names = ["Fela", "Spot", "Patch", "Willy"]
 
+pet_names.append('Claude')
+p pet_names
+
+p pet_names.first
+
+p pet_names.last
+
+pet_names.shift
+p pet_names
 # 1. add “Claude” to the end of the array
 # 2. access the first name in the list
 # 3. access the last name in the list
