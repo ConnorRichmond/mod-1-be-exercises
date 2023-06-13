3083bd02ba2f9ddd9432d00eef6389af3e01183c
Connor Richmond <richmond.connor@gmail.com>
2023-06-12 20:12:27 -0400

    Iteration 2 first half practice

--- practice_independent_challenges/week5/reunion/lib/activity.rb
+++ practice_independent_challenges/week5/reunion/lib/activity.rb
@@ -8,12 +8,22 @@ class Activity
     @participants = {}
   end
 
-  def add_participants(name, cost)
+  def add_participant(name, cost)
     @participants[name] = cost
   end
 
   def total_cost
     @participants.values.sum
+  end
+
+  def split
+    total_cost / @participants.count
+  end
+
+  def owed
+    @participants.transform_values do |money_spent|
+      (split - money_spent)
+    end
   end
 
   
--- practice_independent_challenges/week5/reunion/spec/activity_spec.rb
+++ practice_independent_challenges/week5/reunion/spec/activity_spec.rb
@@ -3,42 +3,69 @@ require './lib/activity'
 
 describe Activity do
 
-  before(:each) do
-    @activity = Activity.new("Brunch")
+  describe 'creating activity with participants' do
+
+    before(:each) do
+      @activity = Activity.new("Brunch")
+    end
+
+    context '#initialize' do
+      it 'exists' do
+        expect(@activity).to be_an(Activity)
+      end
+
+      it 'has attributes' do
+        expect(@activity.name).to eq("Brunch")
+        expect(@activity.participants).to eq({})
+      end
+    end
+
+    context 'participants' do
+      it 'adds participants' do
+        expect(@activity.participants).to eq({})
+
+        @activity.add_participant("Maria", 20)
+        expect(@activity.participants).to eq({"Maria" => 20})
+      end
+    end
+
+    context 'total costs from participants' do
+      it 'calculates total costs' do
+        @activity.add_participant("Maria", 20)
+        expect(@activity.total_cost).to eq(20)
+
+        @activity.add_participant("Luther", 40)
+        expect(@activity.participants).to eq({
+          "Maria" => 20, 
+          "Luther" => 40
+        })
+        expect(@activity.total_cost).to eq(60)
+      end
+    end
   end
 
-  describe '#initialize' do
-    it 'exists' do
-      expect(@activity).to be_an(Activity)
+  describe 'splits costs with participants' do
+    before(:each) do
+      @activity = Activity.new("Brunch")
+      @activity.add_participant("Maria", 20)
+      @activity.add_participant("Luther", 40)
     end
 
-    it 'has attributes' do
-      expect(@activity.name).to eq("Brunch")
-      expect(@activity.participants).to eq({})
+    context 'splits costs with participants' do
+      it 'splits costs' do
+        expect(@activity.total_cost).to eq(60)
+        expect(@activity.split).to eq(30)
+      end
+
+      it 'what is owed between participants' do
+        expect(@activity.owed).to eq({
+          "Maria" => 10,
+          "Luther" => -10
+        })
+      end
     end
-  end
 
-  describe 'participants' do
-    it 'adds participants' do
-      expect(@activity.participants).to eq({})
 
-      @activity.add_participants("Maria", 20)
-      expect(@activity.participants).to eq({"Maria" => 20})
-    end
-  end
-
-  describe 'total costs from participants' do
-    it 'calculates total costs' do
-      @activity.add_participants("Maria", 20)
-      expect(@activity.total_cost).to eq(20)
-
-      @activity.add_participants("Luther", 40)
-      expect(@activity.participants).to eq({
-        "Maria" => 20, 
-        "Luther" => 40
-      })
-      expect(@activity.total_cost).to eq(60)
-    end
   end
 
 
